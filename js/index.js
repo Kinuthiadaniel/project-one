f4569900aed7cec459e0860d53c6ee36cdf0c771
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-04-15 13:14:59 +0300

    update

--- js/index.js
+++ js/index.js
@@ -2,40 +2,42 @@ document.addEventListener("DOMContentLoaded", async (event) => {
   let baseUrl = "http://localhost:3000/vehicles"
   clickFaq()
   const vehi = await getData()
+  firstVehicle(vehi[0])
   displayVehicle(vehi)
-  moreInformation(vehi)
+moreInformation(vehi)
   const searched = document.getElementById("searched")
-  
-    const form = document.getElementById("form")
+
+  const form = document.getElementById("form")
   const search = document.getElementById("search")
+ 
 
 
 
 
-function clickFaq() {
-  const faq = document.querySelector("#faqs")
-  faq.addEventListener("click", (e) => {
-    alert("Still New in the Business we don't have FAQs yet")
-  })
-  return faq;
-}
+  function clickFaq() {
+    const faq = document.querySelector("#faqs")
+    faq.addEventListener("click", (e) => {
+      alert("Still New in the Business we don't have FAQs yet")
+    })
+    return faq;
+  }
 
-function getData() {
-  return fetch(baseUrl, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    }
-  })
-    .then(res => res.json())
-    .then(vehicles => vehicles)
-    
-}
+  function getData() {
+    return fetch(baseUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(vehicles => vehicles)
 
-function displayVehicle(vehicles) {
-  const vehiCard = vehicles.map(vehicle => {
-    return `
+  }
+
+  function displayVehicle(vehicles) {
+    const vehiCard = vehicles.map(vehicle => {
+      return `
     <div class="card">
           <img
             src="${vehicle.image}"
@@ -54,38 +56,77 @@ function displayVehicle(vehicles) {
 
         </div>
   `
-  })
-  const vehicleContainer = document.querySelector("#all-vehicles")
-  vehicleContainer.innerHTML = vehiCard
-}
+    })
+    const vehicleContainer = document.querySelector("#all-vehicles")
+    vehicleContainer.innerHTML = vehiCard
 
-function moreInformation(vehiclesContainer) {
-  const viewDetails = document.querySelector("#view-vehicles")
-  const ul = document.createElement("ul")
+  }
 
-  const moreInfoBtn = document.querySelectorAll(".btn-more")
-  moreInfoBtn.forEach(btn => {
-    btn.addEventListener("click", (event) => {
-      const found = vehiclesContainer.find((element) => element.model === event.target.id)
-      ul.innerHTML = `
+  function moreInformation(vehiclesContainer) {
+    const viewDetails = document.querySelector("#view-vehicles")
+   
+    const ul = document.createElement("ul")
+
+    const moreInfoBtn = document.querySelectorAll(".btn-more")
+    moreInfoBtn.forEach(btn => {
+      btn.addEventListener("click", (event) => {
+      viewDetails.innerHTML=""
+        const found = vehiclesContainer.find((element) => element.model === event.target.id)
+        ul.innerHTML = `
     <li> ${found.model}</li>
     <li> ${found.YoM}</li>
     <li> ${found.price}</li>
     <li> ${found.description}</li>
     <li> <img width = "70% class="mx-auto" src= "${found.image}"/>""</li>
     `
-    viewDetails.appendChild(ul)
+        viewDetails.appendChild(ul)
+
+      })
     })
-  })
-}
+  }
 
-form.addEventListener('submit',(event) =>{
-  event.preventDefault()
-  const foundCars = vehi.filter((found)=>{
+  form.addEventListener('submit', (event) => {
+    searched.innerHTML = ""
+    event.preventDefault()
+    const foundCars = vehi.filter((found) => {
 
-return found.model.toLowerCase().includes(search.value.toLowerCase());
+      return found.model.toLowerCase().includes(search.value.toLowerCase());
+    })
+
+    foundCars.forEach((vehicle) => {
+      const div = document.createElement('div');
+      div.className = "card";
+      div.innerHTML = `
+    <img
+            src="${vehicle.image}"
+            class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${vehicle.model}</h5>
+            <p class="card-text"> Year : ${vehicle.YoM}</p>
+             <form >
+              <div>
+                <input type="number" value="100" min="1" class="col-10"/>
+              </div>
+              <button type="submit" class="btn btn-success btn-sm mx-auto">By Price</button>
+              <button type="button" id = "${vehicle.model}"class="btn btn-primary btn-sm mx-auto btn-more ">More Info</button>
+             </form>
+          </div>
+    `
+      searched.appendChild(div)
+    })
+    moreInformation(foundCars)
   })
-  console.log(foundCars)
-  
-})
+  function firstVehicle(vehicle) {
+    const viewDetails = document.querySelector("#view-vehicles")
+    const ul = document.createElement("ul")
+    ul.innerHTML = `
+    <li> ${vehicle.model}</li>
+    <li> ${vehicle.YoM}</li>
+    <li> ${vehicle.price}</li>
+    <li> ${vehicle.description}</li>
+    <li> <img width = "70% class="mx-auto" src= "${vehicle.image}"/>""</li>
+    `
+    viewDetails.appendChild(ul)
+
+  }
 })
